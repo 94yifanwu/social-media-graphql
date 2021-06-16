d3d8de74dd24ccac16fb3d4bc894a27334dc9367
Yifan Wu <94yifanwu@gmail.com>
2021-06-16 02:12:33 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    MONGODB:'mongodb+srv://yifan:a123456@cluster0.ywgno.mongodb.net/social-app-merng?retryWrites=true&w=majority',
-    
-    SECRET_KEY: 'somethings a df las dflsa f'
-}
-
-
-

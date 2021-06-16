8201b5f96b156f73311e859a231641c6a3a0a218
Yifan Wu <94yifanwu@gmail.com>
2021-06-16 01:40:12 -0700

    finish

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports={
+    MONGODB:'mongodb+srv://yifan:a123456@cluster0.ywgno.mongodb.net/social-app-merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'somethings a df las dflsa f'
+}
+
+
+
